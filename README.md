7448e2273e2d5bd56b3626325439f8c31244e3ed
Peter Linde <plinde@gmail.com>
2017-03-03 13:36:35 -0500

    added in randomized anomaly injection - relative high traffic volume

---
+++ README.md
@@ -0,0 +1,38 @@
+The intention for this utility is to provide a 'training' dataset for Elastic's Prelert product. This may be useful for learning the Prelert product by providing a simple dataset with 'known' anomaly in it.
+
+The utility will generate a linear time-series set of 'HTTP access log' type events into Elasticsearch, along with a semi-randomized "anomaly" event period. In the initial version, this is a "10x" traffic spike.
+
+NOTE: Redis and Logstash are used for some simplicity. Some may argue that requiring Redis/Logstash are actually reducing simplicity and adding complexity, but IMO these tools are already integral to working with the Elastic Stack and actually provide a comfortable abstraction. This code could easily be modified to write directly into ElasticSearch.
+
+* In ```prelert/jobs```, there are example Prelert jobs which can be imported with this dataset.
+
+* In ```logstash/```, there are some Logstash configs which help with ingestion of the events.
+
+#### Expectations
+* Python 2.7 (not tested on Python 3, yet)
+* Elastic Stack 5.2+
+* Redis 3.0+
+
+#### One possible usage
+1. Redis running with defaults (port 6379, localhost). I used 3.2.6 but any 3.x+ version should work fine.
+```~/workspace/redis-3.2.6/src/redis-server```
+
+2. ElasticSearch running with defaults (HTTP 9200, localhost). I used ElasticSearch 5.2.2 but this should work with any version 1.x+.
+```~/workspace/elastic-5.2.2/elasticsearch-5.2.2/bin/elasticsearch```
+
+3. Start logstash using the 'redis-to-es.yml' to pump events from Redis into ElasticSearch. I used Logstash 5.2.2 but again, any version 2.x+ should work fine.
+```~/workspace/elastic-5.2.2/logstash-5.2.2/bin/logstash -f ./redis-to-es.yml```
+
+4. ```pip install -r requirements.txt```
+
+5. ```python generator.py```
+
+Output:
+```
+creating time series for previous 7 days
+creating anomaly time series range within the previous 7 days
+```
+
+Check your Kibana (localhost:5601) and define an index pattern for "smoke_event-*", using @timestamp as the timestamp field. You should see time-series data for the period requested (e.g. 7 days). You should also see a 'spike' up in the histogram. This is your anomaly.
+
+In Prelert (assuming you have this already going), you can upload the job definition inside ```prelert/jobs```. You may need to tune the ElasticSearch version if you are not using 2.x/5.x. Since this is a fixed time-series with a definite endpoint, you can define the Prelert job to terminate (NOT real-time).
--- generator.py
+++ generator.py
@@ -1,33 +1,27 @@
-import json
-from pprint import pprint
 import sys
+import json
 
 import datetime
 from datetime import timedelta
 
+# Logstash HTTP output module
 import ConfigParser
 import requests
 
-import time
-
+# redis output module
 import redis
-
-# cluster_host = sys.argv[1]
-# cluster_port = sys.argv[2]
-# json_loads_1 = sys.argv[3]
-# json_loads_2 = sys.argv[4]
 
 def buildEvent(timestamp = None):
 
     event = {}
 
-    #if we don't have a desired timestamp for the event, use the current time in UTC
+    #if we don't have a desired timestamp passed in for the event, use the current time in UTC
     if timestamp == None:
         event['timestamp'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
     else:
         event['timestamp'] = timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    #make some of these random inputs from seed lists
+    #TODO make some of these random inputs from seed lists
     event['request'] = '/index.html'
     event['response'] = '200'
     event['agent'] = 'Firefox'
@@ -43,15 +37,15 @@ def buildEvent(timestamp = None):
 def buildEventSeries(daysBack = 1):
 
     CURRENT_TIME = datetime.datetime.utcnow()
-    print "End Time, aka CURRENT_TIME: %s" % (CURRENT_TIME.strftime('%Y-%m-%dT%H:%M:%SZ')
+    # print "End Time, aka CURRENT_TIME: %s" % (CURRENT_TIME)
 
     ### Set starting time (e.g. 7 days ago)
     STEP_TIME = CURRENT_TIME - timedelta(days=daysBack)
-    print "Start Time, aka STEP_TIME: %s" % (STEP_TIME.strftime('%Y-%m-%dT%H:%M:%SZ')
+    # print "Start Time, aka STEP_TIME: %s" % (STEP_TIME)
+
     # print STEP_TIME.strftime('%Y-%m-%dT%H:%M:%SZ')
 
     while True:
-
         # break when we reach the current time again
         if STEP_TIME == CURRENT_TIME:
             print "stop!"
@@ -70,13 +64,63 @@ def buildEventSeries(daysBack = 1):
 
     return
 
+
+def buildAnomalyEventSeries(daysBack = 1):
+    # TODO refactor this into a callable function, rather than largely repeat the buildEvent function
+
+    CURRENT_TIME = datetime.datetime.utcnow()
+    # print "End Time, aka CURRENT_TIME: %s" % (CURRENT_TIME.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # e.g. 30 minute anomaly of high traffic somewhere within the relative time series
+
+    anomaly_factor = daysBack * .3
+    ANOMALY_START_TIME = CURRENT_TIME - timedelta(days=anomaly_factor)
+    ANOMALY_END_TIME = ANOMALY_START_TIME + timedelta(minutes=30)
+
+    print "Anomaly Start Time %s" % ANOMALY_START_TIME
+    print "Anomaly End Time %s" %  ANOMALY_END_TIME
+
+    ### Set starting time (e.g. 7 days ago)
+    STEP_TIME = ANOMALY_START_TIME
+
+    while True:
+
+        # print STEP_TIME
+        # print ANOMALY_END_TIME
+
+        # break when we reach the current time again
+        if STEP_TIME == ANOMALY_END_TIME:
+            print "stop!"
+            break
+
+        # step 5 seconds forward for each event
+        STEP_TIME = STEP_TIME + timedelta(seconds=5)
+        # print STEP_TIME.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        #generate 10x more events than normal during the anomaly period
+        i = 0
+        while i < 10:
+            json_event = buildEvent(STEP_TIME)
+            i = i + 1
+
+            # decide which output to use
+            # writeEventToLogstashHTTP(json_event)
+            writeEventToRedis(json_event)
+            # writeEventToRedisDebug(json_event)
+            # writeEventToNull(json_event)
+
+    return
+
 def writeEventToNull(json_event):
     print json_event
+
+def writeEventToRedisDebug(json_event):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r.rpush("smoke_event_DEBUG", json_event)
 
 def writeEventToRedis(json_event):
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     r.rpush("smoke_event", json_event)
-
 
 def writeEventToLogstashHTTP(json_event):
     Config = ConfigParser.ConfigParser()
@@ -89,19 +133,12 @@ def writeEventToLogstashHTTP(json_event):
     # print r.content
 
 def main():
-
-    #daysBack = 1
-    daysBack = 14
+    
+    daysBack = 7
+    print "creating time series for previous 7 days"
     buildEventSeries(daysBack)
-
-
-
-    # while True:
-    #     json_event = buildEvent()
-    #     print json_event
-    #     writeEventToLogstashHTTP(json_event)
-
-
+    print "creating anomaly time series range within the previous 7 days"
+    buildAnomalyEventSeries(daysBack)
 
 if __name__ == "__main__":
     main()
